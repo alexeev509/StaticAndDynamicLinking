3482793489646a27c679604881d022bf73d84acd
Apathy <alexeevy509@gmail.com>
2019-11-12 00:46:12 +0300

    Add new class from Eclipse

---
+++ src/main/java/Test_5_for_eclipse.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test_5_for_eclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hello");
+			List<Integer> list=new ArrayList<Integer>();
+			list.contains(5);
+	}
+
+}
